e05d7cf5d24c84a0e60b7de0f66dafd618cf35b3
Micael Santiago <83787168+micaelsantiago@users.noreply.github.com>
2024-01-29 20:21:07 -0300

    chore: add CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3333;
 const router = require('./router');
+const cors = require('cors');
 
 require('dotenv').config();
 
 app.use(express.json());
+app.use(cors());
 app.use(router);
 app.listen(port);
